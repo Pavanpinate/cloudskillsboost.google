b70b047329aebe32eba9d819aa99e64dc6075644
PAVAN PINATE <149768359+Pavanpinate@users.noreply.github.com>
2025-08-27 21:38:31 +0530

    Add files via upload

---
+++ Introduction to Computer Vision with TensorFlow/loser631.sh
@@ -0,0 +1,46 @@
+
+python --version
+
+python -c "import tensorflow;print(tensorflow.__version__)"
+
+pip install tensorflow tensorflow-datasets google-cloud-logging numpy
+
+pip3 install --upgrade pip
+
+/usr/bin/python3 -m pip install -U google-cloud-logging --user
+
+/usr/bin/python3 -m pip install -U pylint --user
+
+pip install --upgrade tensorflow
+
+wget https://raw.githubusercontent.com/Techcps/GSP-Short-Trick/master/Introduction%20to%20Computer%20Vision%20with%20TensorFlow/requirements.txt
+
+/usr/bin/python3 -m pip install -r requirements.txt
+
+wget https://raw.githubusercontent.com/Techcps/GSP-Short-Trick/master/Introduction%20to%20Computer%20Vision%20with%20TensorFlow/model.py
+
+python model.py
+
+wget https://raw.githubusercontent.com/Techcps/GSP-Short-Trick/master/Introduction%20to%20Computer%20Vision%20with%20TensorFlow/CP1/model.py
+
+python model.py
+
+wget https://raw.githubusercontent.com/Techcps/GSP-Short-Trick/master/Introduction%20to%20Computer%20Vision%20with%20TensorFlow/CP/model.py
+
+python model.py
+
+wget https://raw.githubusercontent.com/Techcps/GSP-Short-Trick/master/Introduction%20to%20Computer%20Vision%20with%20TensorFlow/callback_model.py
+
+python callback_model.py
+
+wget https://raw.githubusercontent.com/Techcps/GSP-Short-Trick/master/Introduction%20to%20Computer%20Vision%20with%20TensorFlow/updated_model_1.py
+
+python updated_model_1.py
+
+wget https://raw.githubusercontent.com/Techcps/GSP-Short-Trick/master/Introduction%20to%20Computer%20Vision%20with%20TensorFlow/updated_model_2.py
+
+python updated_model_2.py
+
+wget https://raw.githubusercontent.com/Techcps/GSP-Short-Trick/master/Introduction%20to%20Computer%20Vision%20with%20TensorFlow/updated_model_3.py
+
+python updated_model_3.py
