63b56be6cead6e7b1f5e06ce3360a95fbea792f6
PAVAN PINATE <149768359+Pavanpinate@users.noreply.github.com>
2025-09-10 19:53:48 +0530

    Update loser152.sh

--- Securing a Cloud SQL for PostgreSQL Instance/loser152.sh
+++ Securing a Cloud SQL for PostgreSQL Instance/loser152.sh
@@ -1,78 +1,9 @@
-
 gcloud auth list
 
-export PROJECT_ID=$DEVSHELL_PROJECT_ID
+export REGION=$(gcloud compute project-info describe --format="value(commonInstanceMetadata.items[google-compute-default-region])")
 
-cat > inspect-request.json <<EOF_CP
-{
-  "item":{
-    "value":"My phone number is (206) 555-0123."
-  },
-  "inspectConfig":{
-    "infoTypes":[
-      {
-        "name":"PHONE_NUMBER"
-      },
-      {
-        "name":"US_TOLLFREE_PHONE_NUMBER"
-      }
-    ],
-    "minLikelihood":"POSSIBLE",
-    "limits":{
-      "maxFindingsPerItem":0
-    },
-    "includeQuote":true
-  }
-}
-EOF_CP
+echo $REGION
 
-ACCESS_TOKEN=$(gcloud auth application-default print-access-token)
+gcloud config set compute/region $REGION
 
-curl -s \
-  -H "Authorization: Bearer $ACCESS_TOKEN" \
-  -H "Content-Type: application/json" \
-  https://dlp.googleapis.com/v2/projects/$PROJECT_ID/content:inspect \
-  -d @inspect-request.json -o inspect-output.txt
-
-
-cat inspect-output.txt
-
-gsutil cp inspect-output.txt gs://$PROJECT_ID-bucket
-
-
-
-cat > new-inspect-file.json <<EOF_CP
-{
-  "item": {
-     "value":"My email is test@gmail.com",
-   },
-   "deidentifyConfig": {
-     "infoTypeTransformations":{
-          "transformations": [
-            {
-              "primitiveTransformation": {
-                "replaceWithInfoTypeConfig": {}
-              }
-            }
-          ]
-        }
-    },
-    "inspectConfig": {
-      "infoTypes": {
-        "name": "EMAIL_ADDRESS"
-      }
-    }
-}
-EOF_CP
-
-
-curl -s \
-  -H "Authorization: Bearer $(gcloud auth print-access-token)" \
-  -H "Content-Type: application/json" \
-  https://dlp.googleapis.com/v2/projects/$PROJECT_ID/content:deidentify \
-  -d @new-inspect-file.json -o redact-output.txt
-
-cat redact-output.txt
-
-gsutil cp redact-output.txt gs://$PROJECT_ID-bucket
-
+gcloud sql instances create myinstance --database-version=POSTGRES_15 --tier=db-custom-2-7680 --region=$REGION --storage-type=SSD --storage-size=250GB
